90b73842d1c333a0545e643b67522f21276b00e5
VKohai <kohai.entertainment@gmail.com>
2024-11-28 19:35:00 +0300

    First commit. Add reading the data

---
+++ src/Hakaton/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = File.ReadAllLines("in.txt");
+// N - Suppliers, M - Consumers
+var (N, M) = input[0].Split().Select(int.Parse).ToArray() switch { var arr => (arr[0], arr[1]) };
+var supplies = input[1].Split().Select(int.Parse).ToArray();
+var demands = input[2].Split().Select(int.Parse).ToArray();
+
+var costs = new int[N, M];
+
+Parallel.For(0, N, i =>
+{
+    var costLine = input[3 + i].Split().Select(int.Parse).ToArray();
+    for (int j = 0; j < M; ++j)
+        costs[i, j] = costLine[j];
+});
---
+++ tests/Hakaton.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Hakaton.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
